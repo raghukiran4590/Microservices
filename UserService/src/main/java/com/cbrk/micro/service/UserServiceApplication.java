e277031c1802a48de4a5aa8223189383f22e4964
Raghu Kiran <raghukiran4590@gmail.com>
2025-01-20 04:39:00 -0500

    Service Discovery Clients

--- UserService/src/main/java/com/cbrk/micro/service/UserServiceApplication.java
+++ UserService/src/main/java/com/cbrk/micro/service/UserServiceApplication.java
@@ -2,6 +2,8 @@ package com.cbrk.micro.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -9,5 +11,11 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
+
+/*	Defined in the config package
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}*/
 
 }
